b4c70b150868ae6a1facd81499fe9a320d071ecf
Manmeet Rahi <manmeetrahi@Manmeets-MacBook-Air.local>
2025-09-21 03:11:24 +0530

    Added DB

--- models/Employee.js
+++ models/Employee.js
@@ -1,10 +1,17 @@
+// models/Employee.js
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const employeeSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  phone: String,
-  password: String,
-  department: String
-});
+const employeeSchema = new Schema(
+  {
+    name: { type: String },
+    email: { type: String, unique: true, sparse: true },
+    phone: { type: String, unique: true, sparse: true },
+    password: { type: String, required: true },
+    department: { type: String },
+    role: { type: String, default: "employee" },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Employee", employeeSchema);
