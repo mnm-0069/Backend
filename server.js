ed09ab4981ae99d30c5d6a85d138fe3066283b5e
Manmeet Rahi <manmeetrahi@Manmeets-MacBook-Air.local>
2025-09-22 13:12:47 +0530

    UPD

--- server.js
+++ server.js
@@ -341,18 +341,18 @@ app.get("/issue", async (req, res) => {
   }
 });
 
-// GET specific issue by ID
-router.get("/issue/:_id", async (req, res) => {
-  try {
-    const issue = await Issue.findById(req.params._id);
-    if (!issue) {
-      return res.status(404).json({ message: "Issue not found" });
-    }
-    res.json(issue);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// // GET specific issue by ID
+// router.get("/issue/:_id", async (req, res) => {
+//   try {
+//     const issue = await Issue.findById(req.params._id);
+//     if (!issue) {
+//       return res.status(404).json({ message: "Issue not found" });
+//     }
+//     res.json(issue);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
 
 // Assign issue to employee
